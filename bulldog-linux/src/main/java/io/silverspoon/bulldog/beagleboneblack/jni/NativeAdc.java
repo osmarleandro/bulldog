d45e6a6c798a4e53a04b7e9e741a3b1b71f8ac03
Osmar Leandro <osmarleandrods@gmail.com>
2020-02-16 12:49:50 -0300

    ref: Extrac method NativeAdc::initialize() at line 67

--- bulldog-linux/src/main/java/io/silverspoon/bulldog/beagleboneblack/jni/NativeAdc.java
+++ bulldog-linux/src/main/java/io/silverspoon/bulldog/beagleboneblack/jni/NativeAdc.java
@@ -64,14 +64,18 @@ public static final int BBBIO_ADC_STEP_MODE_SW_ONE_SHOOT = NativePwm.BBBIO_ADC_S
          return;
       }
       setup();
-      isInitialized = true;
+      extracted();
+   }
+
+private static void extracted() {
+	isInitialized = true;
       Runtime.getRuntime().addShutdownHook(new Thread() {
          @Override
          public void run() {
             deinitialize();
          }
       });
-   }
+}
 
    public static void deinitialize() {
       if (isInitialized) {
